51c3d8d78e0415dff2f5587a380f23b1ccef93f7
sora_watabe <wataso0404@gmail.com>
2021-11-20 10:19:33 +0900

    master

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ line_bot_api=LineBotApi(CANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID=info['USER_ID']
-    messages=TextSendMessage(text='おはよう、いい朝だ～ \n今日も一緒にでがんばるぞー ')
+    messages=TextSendMessage(text='おはよう、いい朝だ～ \n今日も一緒にがんばるぞー!!')
     line_bot_api.push_message(USER_ID,messages=messages)
     
 if __name__ == "__main__":
